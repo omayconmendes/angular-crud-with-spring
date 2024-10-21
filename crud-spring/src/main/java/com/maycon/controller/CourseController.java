aef05679170158499247bf4e10682d46da354a85
Maycon Mendes <maycon.mendes@senior.com.br>
2024-10-21 10:39:11 -0300

    feat: Cria métodos para listar cursos por Id e método para Update de um curso.

--- crud-spring/src/main/java/com/maycon/controller/CourseController.java
+++ crud-spring/src/main/java/com/maycon/controller/CourseController.java
@@ -22,10 +22,29 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
 //        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
