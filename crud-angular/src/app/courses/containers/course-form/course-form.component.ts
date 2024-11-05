31a98103b0a95fd6f36986edbd82829e6cdfa2df
Maycon Mendes <maycon.mendes@senior.com.br>
2024-11-04 23:52:17 -0300

    feat: [FRONT-END] - Inclui validações no formulário de adicionar curso.

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,8 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+    category: ['', [Validators.required]]
   });
 
   constructor(
@@ -34,8 +34,12 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.coursesService.save(this.form.value)
-      .subscribe(result => this.onSuccess(), error => this.onError());
+    if (this.form.valid) {
+      this.coursesService.save(this.form.value)
+        .subscribe(result => this.onSuccess(), error => this.onError());
+    } else {
+      this.onError();
+    }
   }
 
   onCancel() {
@@ -49,6 +53,25 @@ export class CourseFormComponent implements OnInit {
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso', '', {duration: 5000})
+  }
+
+  public getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 5;
+      return `Tamanho máximo precisa ser de ${requiredLength} caracteres`
+    }
+
+    return 'Campo inválido'
   }
 
 }
